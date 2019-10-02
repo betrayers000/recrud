5fc1ce0702867bfa8e0b47d54b8603bac5d793c4
jw <betrayers000@gmail.com>
2019-10-02 15:56:03 +0900

    index modfiy

--- todos/models.py
+++ todos/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=50)
-    due_date = models.DateField
+    due_date = models.DateField()
